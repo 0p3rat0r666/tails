0093e0dda3be144434393b9d474a663dd2f5caac
Console.log('im, MHAzcmF0MHI2NjY=') <106628238+0p3rat0r666@users.noreply.github.com>
2023-01-24 12:15:04 -0600

    Tails

---
+++ Tails.py
@@ -0,0 +1,21 @@
+#   {Fore.YELLOW} ^           ^
+#   /v\         /v\
+#  /vv\________/vv\
+#  |              |
+#  |    \  /      |
+# /    |    |     \
+#<\      v        />
+# <\             />
+#  <\___________/>
+
+tails = f"""    {Fore.YELLOW}^          ^
+   /v\  _/\_  /v\
+  /vv\_/____\/vv\
+ |FFFFFFFFFFFFFF|
+ |F{Fore.WHITE}EEE{Fore.BLACK}\{Fore.WHITE}EE{Fore.BLACK}/{Fore.WHITE}EEE{Fore.YELLOW}FFF|
+ /F{Fore.WHITE}EE{Fore.BLUE}|{Fore.WHITE}EEEE{Fore.BLUE}|{Fore.WHITE}EE{Fore.YELLOW}FFF\
+<\F{Fore.WHITE}EEEEE{Fore.BLACK}v{Fore.WHITE}EEEE{Fore.YELLOW}FFF/>
+ <\FFFFFFFFFFFF/>
+  <\__________/>
+"""
+print(tails)
