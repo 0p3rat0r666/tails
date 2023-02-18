cb7f955509a7792c1657ce7b6f8c4d0f9302a399
Console.log('im, MHAzcmF0MHI2NjY=') <106628238+0p3rat0r666@users.noreply.github.com>
2023-02-18 10:22:17 -0600

    updated tails

---
+++ tails2.py
@@ -0,0 +1,16 @@
+from colorama import Fore, Style
+
+tails2 = f"""
+    ^              ^
+   |VV\           /V\
+   |VVV\    /\__ /VVV\
+   |VVVV\ / __\_/VVVVV\
+   |HHHHHH\/HHHHHHHHHH|
+   |HHHH\HHHHHH/HHHHHH|
+   |GEE|EEEVEEEE|EEGGG|
+ </GGEEEEEEEEEEEEEEGGG\>
+ <\GGGGEEEEVEEEEGGGGGG/>
+  <\GGGGGEEEEEGGGGGGG/>
+    \G_____________G/
+"""
+print(tails2)
